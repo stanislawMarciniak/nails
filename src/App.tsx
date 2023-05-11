73807b62dfe6a23f610fa661bb14a37e655c9829
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-05-11 22:26:49 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import Calendar from "./pages/Calendar";
+
+function App() {
+  return;
+  <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cennik" element={<Pricing />} />
+        <Route path="/kalendarz" element={<Calendar />} />
+      </Routes>
+    </BrowserRouter>
+  </div>;
+}
+
+export default App;
