2f240f4dad991b5476c924a50b7d10cb28ad135e
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-05-12 22:56:07 +0200

    calendar setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import Pricing from "./pages/Pricing";
 import Calendar from "./pages/Calendar";
 
 function App() {
-  return;
-  <div>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cennik" element={<Pricing />} />
-        <Route path="/kalendarz" element={<Calendar />} />
-      </Routes>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cennik" element={<Pricing />} />
+          <Route path="/kalendarz" element={<Calendar />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,5 +1,17 @@
+import ReactCalendar from "react-calendar";
+import "./Calendar.css";
+
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div>
+      <ReactCalendar
+        className="react-calendar"
+        minDate={new Date()}
+        view="month"
+        onClickDay={(date) => console.log(date)}
+      />
+    </div>
+  );
 };
 
 export default Calendar;
