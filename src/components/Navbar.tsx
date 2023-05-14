5b3d366678abd44b149cfb73166594fce5744b96
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-05-14 22:52:16 +0200

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="text-white py-4 px-8 items-center flex justify-between bg-gray-800">
-      <Link to="/" className="text-5xl">
+    <nav className="flex items-stretch justify-between h-20 px-8 text-white bg-gray-800">
+      <Link to="/" className="flex items-center text-5xl">
         Home
       </Link>
-      <div className="h-full">
+      <div className="flex">
         <Link
           to="/kalendarz"
-          className="py-4 px-8 text-4xl  h-full hover:bg-gray-600"
+          className="flex items-center px-8 text-4xl hover:bg-gray-600"
         >
           Kalendarz
         </Link>
         <Link
           to="/cennik"
-          className="py-4 px-8 text-4xl h-full hover:bg-gray-600"
+          className="flex items-center px-8 text-4xl hover:bg-gray-600"
         >
           Cennik
         </Link>
