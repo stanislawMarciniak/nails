8049ed81191bc28cc0d6474dad61864ea40684d3
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-08-04 19:41:47 +0200

    dropdown logic

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import "./Dropdown.css";
 
 const Dropdown = ({ selected, setSelected, id, active, setActive, data }) => {
@@ -13,18 +14,31 @@ const Dropdown = ({ selected, setSelected, id, active, setActive, data }) => {
       </div>
       {active === id && (
         <div className="dropdown-content">
-          {data.map((service, id) => (
-            <div
-              key={id}
-              onClick={() => {
-                setSelected(service);
-                setActive(-1);
-              }}
-              className="dropdown-item"
-            >
-              {service}
-            </div>
-          ))}
+          {id === 0
+            ? formatedData.map((service, id) => (
+                <div
+                  key={id}
+                  onClick={() => {
+                    setSelected(service);
+                    setActive(-1);
+                  }}
+                  className="dropdown-item"
+                >
+                  {service.name}
+                </div>
+              ))
+            : formatedData.map((time, id) => (
+                <div
+                  key={id}
+                  onClick={() => {
+                    setSelected(time);
+                    setActive(-1);
+                  }}
+                  className="dropdown-item"
+                >
+                  {format(time, "kk:mm")}
+                </div>
+              ))}
         </div>
       )}
     </div>
