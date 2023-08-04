2d9523c088173a01428462bd9bdafdd0edf7e919
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-08-04 23:50:05 +0200

    confiug

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
-export const OPENING_HOURS_BEGINNING = 15;
-export const OPENING_HOURS_END = 20;
+export const OPENING_HOURS_BEGINNING = 9;
+export const OPENING_HOURS_END = 22;
 export const OPENING_HOURS_INTERVAL = 30; // in minutes
 export const RESPONSIVENESS_1 = {
   superLargeDesktop: {
