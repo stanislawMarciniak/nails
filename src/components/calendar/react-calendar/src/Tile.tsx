fccda0bb4be6355890a01e5b3b6536dd6c95992e
Stanisław Marciniak <marciniak13ee@gmail.com>
2023-08-26 22:57:54 +0200

    tiles

--- src/components/calendar/react-calendar/src/Tile.tsx
+++ src/components/calendar/react-calendar/src/Tile.tsx
@@ -8,6 +8,9 @@ import type {
   TileDisabledFunc,
   View,
 } from "./shared/types.js";
+import supabase from "../../../../config/supabaseClient.js";
+import { format, parse } from "date-fns";
+import { formatLongDate } from "./shared/dateFormatter.js";
 
 type TileProps = {
   activeStartDate: Date;
@@ -66,27 +69,55 @@ export default function Tile(props: TileProps) {
       : tileContentProps;
   }, [activeStartDate, date, tileContentProps, view]);
 
-  const data = [
-    { date: "23 sierpnia 2023", dateType: "full" },
-    { date: "24 sierpnia 2023", dateType: "full" },
-    { date: "25 sierpnia 2023", dateType: "full" },
-    { date: "10 sierpnia 2023", dateType: "full" },
-    { date: "20 września 2023", dateType: "freeDay" },
-    { date: "20 sierpnia 2023", dateType: "freeDay" },
-    { date: "13 sierpnia 2023", dateType: "freeDay" },
-    { date: "6 sierpnia 2023", dateType: "freeDay" },
-    { date: "27 sierpnia 2023", dateType: "freeDay" },
-  ];
+  // const data = [
+  //   { date: "23 sierpnia 2023", dateType: "full" },
+  //   { date: "24 sierpnia 2023", dateType: "full" },
+  //   { date: "25 sierpnia 2023", dateType: "full" },
+  //   { date: "10 sierpnia 2023", dateType: "full" },
+  //   { date: "20 września 2023", dateType: "freeDay" },
+  //   { date: "20 sierpnia 2023", dateType: "freeDay" },
+  //   { date: "13 sierpnia 2023", dateType: "freeDay" },
+  //   { date: "6 sierpnia 2023", dateType: "freeDay" },
+  //   { date: "27 sierpnia 2023", dateType: "freeDay" },
+  // ];
+
   const [type, setType] = useState("");
+  const [formattedData, setFormattedData] = useState([]);
+
   useEffect(() => {
-    const isDate = data.find((data) => data.date === formatAbbr(locale, date));
+    const fetchData = async () => {
+      const { data, error } = await supabase
+        .from("special_days")
+        .select("*")
+        .or("count.eq.-1,count.gte.3");
+
+      const changedData = data?.map((day) => {
+        const date = formatLongDate(locale, day.day);
+
+        return {
+          date: date,
+          dateType: day.count === -1 ? "freeDay" : "full",
+        };
+      });
+      setFormattedData(changedData);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const isDate = formattedData?.find(
+      (data) => data.date === formatAbbr(locale, date)
+    );
     const wasIt = minDate && minDateTransform(minDate) > date;
     const wasAlready = wasIt ? "wasAlready" : "";
 
     if (isDate) {
       setType(isDate.dateType + " " + wasAlready);
-    } else setType(wasAlready);
-  }, []);
+    } else {
+      setType(wasAlready);
+    }
+  }, [formattedData]);
 
   return (
     <button
