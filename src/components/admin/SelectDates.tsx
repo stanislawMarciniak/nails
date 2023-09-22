961aa12af0081b895cf42c84a609f3f7c6ca5a33
Stanisław Marciniak <marciniak.dev@gmail.com>
2023-09-22 23:32:54 +0200

    dates interval

--- src/components/admin/SelectDates.tsx
+++ src/components/admin/SelectDates.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import { DateRangePicker } from "react-date-range";
 import { Center, Select, Stack, useMediaQuery } from "@chakra-ui/react";
 import { addDays } from "date-fns";
+import { pl } from "date-fns/locale";
 
 const SelectDates = () => {
   const [isLargerThan1000] = useMediaQuery("(min-width: 1000px)");
@@ -14,11 +15,23 @@ const SelectDates = () => {
       key: "selection",
     },
   ]);
-  console.log(state);
+
+  const getDatesBetween = (range) => {
+    const dates = [];
+    let currentDate = new Date(range.startDate);
+
+    while (currentDate <= range.endDate) {
+      dates.push(new Date(currentDate));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return dates;
+  };
   return (
     <Center>
-      <Stack>
+      <Stack align={"center"}>
         <DateRangePicker
+          locale={pl}
           className="shadow-2xl date-range-picker"
           onChange={(item) => setState([item.selection])}
           minDate={new Date()}
@@ -30,11 +43,27 @@ const SelectDates = () => {
           direction={isLargerThan1000 ? "horizontal" : "vertical"}
           rangeColors={["#927979"]}
         />
-        <Select placeholder="Wybierz typ dni" variant={"filled"} w={"2xs"}>
-          <option value="option1">Dni wolne od pracy</option>
-          <option value="option2">Dni zapełnione</option>
-          <option value="option3">Dni całkowicie puste</option>
+        <Select
+          placeholder="Wybierz typ dni"
+          variant={"filled"}
+          w={"2xs"}
+          position={"relative"}
+          right={"55%"}
+          top={"4"}
+          border={"1px"}
+          opacity={0.5}
+        >
+          <option value="free">Dni wolne od pracy</option>
+          <option value="full">Dni zapełnione</option>
+          <option value="default">Dni całkowicie puste</option>
         </Select>
+        <button
+          className="px-8 pt-4 pb-3 mt-20 text-2xl rounded-full shadow-xl opacity-80 w-max bg-thirdColor josefin-light "
+          type="button"
+          onClick={() => console.log(getDatesBetween(state[0]))}
+        >
+          ZAPISZ
+        </button>
       </Stack>
     </Center>
   );
